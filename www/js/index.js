a8148752b9a8351ad22cf29cf1b645d0afad8bab
fignon58 <g.poncelet@elistair.com>
2016-05-17 15:08:39 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
        // app.receivedEvent('deviceready');
-		setInterval(F_Send_Request,500) ;
+		setInterval(F_Send_Request,750) ;
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
