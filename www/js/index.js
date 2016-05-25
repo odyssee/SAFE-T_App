a1828e03b11bc7ec3f63ceba5ccc0ecddcbd6340
fignon58 <g.poncelet@elistair.com>
2016-05-25 17:18:11 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -191,7 +191,7 @@ function F_Send_Request()
 			document.getElementById('VITESSE').innerHTML = reponse_text_Array[9]+"m/s" ;
 			document.getElementById('LONGUEUR').innerHTML = reponse_text_Array[7]+'m' ;
 			//document.getElementById('COUPLE').innerHTML = reponse_text_Array[8]+'%' ;
-			document.getElementById('COUPLE').innerHTML = flag_alarme_temperature_systeme_detecte_1 + ' ' + flag_alarme_temperature_systeme_detecte  ;
+			document.getElementById('COUPLE').innerHTML = chaine_alerte  ;
 			document.getElementById('HEURES_TOTALES').innerHTML = reponse_text_Array[3] ;
 			
 			
